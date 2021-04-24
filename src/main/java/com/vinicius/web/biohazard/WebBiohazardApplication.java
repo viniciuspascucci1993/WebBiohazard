481e8e04c48875b3fe498464ae211719899d10da
Vinicius Pascucci <vinicius.pascucci1@gmail.com>
2021-04-24 12:50:59 -0300

    First Commit on Project Biohazard

---
+++ src/main/java/com/vinicius/web/biohazard/WebBiohazardApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.web.biohazard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBiohazardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBiohazardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinicius/web/biohazard/WebBiohazardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.web.biohazard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBiohazardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
