5c24c0adbd5924bbf467855ccecf3ffcd4165637
Vinicius Pascucci <vinicius.pascucci1@gmail.com>
2021-04-24 13:47:37 -0300

    Create Domain classes

---
+++ src/main/java/com/vinicius/web/biohazard/domain/Weapon.java
@@ -0,0 +1,199 @@
+package com.vinicius.web.biohazard.domain;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+/**
+ * Classe responsável por conter os atributos da arma.
+ * @author Vinicius-PC - Vinicius Torres Pascucci.
+ */
+public class Weapon implements Serializable {
+
+	/**
+	 * Serial version UID.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Represents the weapon's identifier.
+	 */
+	private Integer id;
+	
+	/**
+	 * Represents the name of the weapon.
+	 */
+	private String name;
+	
+	/**
+	 * Represents the price of the weapon.
+	 */
+	private Double price;
+	
+	/**
+	 * Represents the manufactor's name.
+	 */
+	private String manufactor;
+	
+	/**
+	 * Represents the date was created the weapon.
+	 */
+	private LocalDate dateManufacturing;
+	
+	/**
+	 * Represent one weapon's category.
+	 */
+	private WeaponCategory weaponCategory;
+	
+	/**
+	 * The constructor empty;
+	 */
+	public Weapon() { }
+
+	/**
+	 * The constructor using fields.
+	 * @param id - Integer - identifier of the weapon.
+	 * @param name - String - name of the weapon.
+	 * @param price - Double - price of the weapon.
+	 * @param manufactor - String - manufactor's name.
+	 * @param dateManufacturing - LocalDate - Date was created the weapon.
+	 * @param weaponCategory - WeaponCategory - weapon category.
+	 */
+	public Weapon(Integer id, String name, Double price, String manufactor, LocalDate dateManufacturing,
+			WeaponCategory weaponCategory) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.manufactor = manufactor;
+		this.dateManufacturing = dateManufacturing;
+		this.weaponCategory = weaponCategory;
+	}
+
+	/**
+	 * get() method.
+	 * @return identifier of the weapon.
+	 */
+	public Integer getId() {
+		return this.id;
+	}
+
+	/**
+	 * set() method.
+	 * @param id - identifier as param.
+	 */
+	public void setId(final Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * get() method.
+	 * @return weapons's name.
+	 */
+	public String getName() {
+		return this.name;
+	}
+
+	/**
+	 * set() method.
+	 * @param name - name as param.
+	 */
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	/**
+	 * get() method.
+	 * @return weapon's price.
+	 */
+	public Double getPrice() {
+		return this.price;
+	}
+
+	/**
+	 * set() method.
+	 * @param price - price as param.
+	 */
+	public void setPrice(final Double price) {
+		this.price = price;
+	}
+
+	/**
+	 * get() method.
+	 * @return manufactor's name.
+	 */
+	public String getManufactor() {
+		return this.manufactor;
+	}
+
+	/**
+	 * set() method.
+	 * @param manufactor - manufactor as param.
+	 */
+	public void setManufactor(final String manufactor) {
+		this.manufactor = manufactor;
+	}
+
+	/**
+	 * get() method.
+	 * @return manufactoring date.
+	 */
+	public LocalDate getDateManufacturing() {
+		return this.dateManufacturing;
+	}
+
+	/**
+	 * set() method.
+	 * @param dateManufacturing - manufactoringDate as param.
+	 */
+	public void setDateManufacturing(final LocalDate dateManufacturing) {
+		this.dateManufacturing = dateManufacturing;
+	}
+
+	/**
+	 * get() method.
+	 * @return weaponCategory.
+	 */
+	public WeaponCategory getWeaponCategory() {
+		return this.weaponCategory;
+	}
+
+	/**
+	 * set() method.
+	 * @param weaponCategory - WeaponCategory as param.
+	 */
+	public void setWeaponCategory(final WeaponCategory weaponCategory) {
+		this.weaponCategory = weaponCategory;
+	}
+
+	/**
+	 * Method hasCode() to generate a hashCode from an id.
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	/**
+	 * Method equals() to compare an object only by id.
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Weapon other = (Weapon) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
---
+++ src/main/java/com/vinicius/web/biohazard/domain/WeaponCategory.java
@@ -0,0 +1,152 @@
+package com.vinicius.web.biohazard.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Classe responsável por conter os atributos da categoria da arma, 
+ * @author Vinicius-PC - Vinicius Torres Pascucci.
+ */
+public class WeaponCategory implements Serializable {
+
+	/**
+	 * Serial version UID.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Represents the category weapon's identifier.
+	 */
+	private Integer id;
+	
+	/**
+	 * Represents the name of the category.
+	 */
+	private String name;
+	
+	/**
+	 * Represents the description of the weapon.
+	 */
+	private String description;
+	
+	/**
+	 * Represent a List of weapons.
+	 */
+	private List<Weapon> weapons = new ArrayList<Weapon>();
+	
+	/**
+	 * The constructor empty.
+	 */
+	public WeaponCategory() {  }
+
+	/**
+	 * The constructor using fields;
+	 * @param id - Integer - Identifier of the category weapon.
+	 * @param name - String - Name of the category weapon.
+	 * @param description - String - Description of the ctegory weapon.
+	 */
+	public WeaponCategory(Integer id, String name, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
+
+	/**
+	 * get() method.
+	 * @return the identifier of the category weapon.
+	 */
+	public Integer getId() {
+		return this.id;
+	}
+
+	/**
+	 * set() method.
+	 * @param id - Identifier as param.
+	 */
+	public void setId(final Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * get() method.
+	 * @return the name of the category weapon.
+	 */
+	public String getName() {
+		return this.name;
+	}
+
+	/**
+	 * set() method.
+	 * @param name - Name as param.
+	 */
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	/**
+	 * get() method.
+	 * @return the description of the category weapon.
+	 */
+	public String getDescription() {
+		return this.description;
+	}
+
+	/**
+	 * set() method.
+	 * @param description - Description as param.
+	 */
+	public void setDescription(final String description) {
+		this.description = description;
+	}
+
+	/**
+	 * get() method.
+	 * @return a List of category Weapon.
+	 */
+	public List<Weapon> getWeapons() {
+		return this.weapons;
+	}
+
+	/**
+	 * set() method.
+	 * @param weapons - List of weapon's as param.
+	 */
+	public void setWeapons(List<Weapon> weapons) {
+		this.weapons = weapons;
+	}
+
+	/**
+	 * Method hasCode() to generate a hashCode from an id.
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	/**
+	 * Method equals() to compare an object only by id.
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		WeaponCategory other = (WeaponCategory) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
